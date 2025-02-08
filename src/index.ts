01cd65890c46bb31dbeac6dc8e166c62bd4e8e5b
s-renren <s1f102301156@iniad.org>
2025-02-08 23:45:32 +0900

    get, post, put, deleteの基本的な書き方

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,81 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+//仮置き
+let blogPosts = [
+  {
+    id: "1",
+    title: "Blog1",
+    content: "Blog Posts",
+  },
+  {
+    id: "2",
+    title: "Blog2",
+    content: "Blog Posts2",
+  },
+  {
+    id: "3",
+    title: "Blog3",
+    content: "Blog Posts3",
+  },
+];
+// 出力する際のJSONを見やすくできる
+app.use("*", prettyJSON());
 
-export default app
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/posts", (c) => c.json({ posts: blogPosts }));
+
+app.get("/posts/:id", (c) => {
+  const id = c.req.param("id");
+  const post = blogPosts.find((p) => p.id === id);
+
+  if (post) {
+    return c.json(post);
+  } else {
+    return c.json({ messae: "not found this page" }, 404);
+  }
+});
+
+app.post("/posts", async (c) => {
+  const { title, content } = await c.req.json<{
+    title: string;
+    content: string;
+  }>();
+  const newPost = { id: String(blogPosts.length + 1), title, content };
+  blogPosts = [...blogPosts, newPost];
+  return c.json(newPost, 201);
+});
+
+app.put("/posts/:id", async (c) => {
+  const id = c.req.param("id");
+  const index = blogPosts.findIndex((p) => p.id === id);
+
+  if (index === -1) {
+    return c.json({ message: "not found" }, 404);
+  }
+
+  const { title, content } = await c.req.json();
+  blogPosts[index] = { ...blogPosts[index], title, content };
+
+  return c.json(blogPosts[index]);
+});
+
+app.delete("/posts/:id", (c) => {
+  const id = c.req.param("id");
+  const index = blogPosts.findIndex((p) => p.id === id);
+
+  if (index === -1) {
+    return c.json({ message: "not found" }, 404);
+  }
+
+  blogPosts = blogPosts.filter((p) => p.id !== id);
+
+  return c.json({ message: "delete success" });
+});
+
+export default app;
