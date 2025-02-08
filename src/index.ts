4e79d988e29cbddf23d52b77975c1811ce0c8407
s-renren <s1f102301156@iniad.org>
2025-02-09 00:02:42 +0900

    認証されていないと操作できないように

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
-import blogs from "./blogs/blogs";
+import pages from "./blogs/blogs";
+import auth from "./auth/auth";
+import { basicAuth } from "hono/basic-auth";
 
 const app = new Hono();
 
 // 出力する際のJSONを見やすくできる
 app.use("*", prettyJSON());
-app.route("/posts", blogs);
+app.use(
+  "/auth/*",
+  basicAuth({
+    username: "renren",
+    password: "1234renren",
+  })
+);
+
+app.route("/posts", pages);
+app.route("auth", auth);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
